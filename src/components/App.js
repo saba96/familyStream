6ec9650a1dd6de21dd1456341c4985e39bb8adcd
Michael Sodaro <xtra.zero@gmail.com>
2017-08-11 10:49:48 +0900

    Add Stream component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import Stream from './Stream';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        HELLO WORLD
+        <Stream />
       </div>
     );
   }
---
+++ src/components/Stream.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Stream extends Component {
+  render() {
+    return (
+      <div>
+        IMAGE STREAM HERE
+      </div>
+    );
+  }
+}
+
+export default Stream;
