2a8ced285b274d00718edb31d71631a869a2a8aa
Michael Sodaro <xtra.zero@gmail.com>
2017-08-11 21:04:35 +0900

    Refactor to not pass 'this' around

--- src/components/Stream.jsx
+++ src/components/Stream.jsx
@@ -1,55 +1,50 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const dragover_handler = (props, event) => {
-  event.preventDefault();
-  console.log('DRAGOVER EVENT', event);
-}
-
-const drop_handler = (props, event) => {
-  event.preventDefault();
-  console.log('RECEIVED IMAGE EVENT', event);
-
-  let dt = event.dataTransfer;
-  let files = [];
-
-  // If using the new implementation
-  if (dt.items) {
-    for (let i=0; i < dt.items.length; i++) {
-      if (dt.items[i].kind === "file") {
-        files.push(dt.items[i].getAsFile());
-      }
-    }
-  } else { // In case of old implementation
-    for (let i=0; i < dt.files.length; i++) {
-      console.log("... file[" + i + "].name = " + dt.files[i].name);
-    }
-  }
-
-  console.log(props);
-  props.onReceiveImages(files);
-}
-
-const dragend_handler = (props, event) => {
-  console.log('DRAGEND EVENT');
-  // Remove all of the drag data
-  var dt = event.dataTransfer;
-  if (dt.items) {
-    // Use DataTransferItemList interface to remove the drag data
-    for (var i = 0; i < dt.items.length; i++) {
-      dt.items.remove(i);
-    }
-  } else {
-    // Use DataTransfer interface to remove the drag data
-    event.dataTransfer.clearData();
-  }
-}
 
 class Stream extends Component {
-  constructor(props) {
-    super(props);
+  dragover_handler = (event) => {
+    event.preventDefault();
+    console.log('DRAGOVER EVENT', event);
+  }
 
-    console.log(props);
+  drop_handler = (event) => {
+    event.preventDefault();
+    console.log('RECEIVED IMAGE EVENT', event);
+
+    let dt = event.dataTransfer;
+    let files = [];
+
+    // If using the new implementation
+    if (dt.items) {
+      for (let i=0; i < dt.items.length; i++) {
+        if (dt.items[i].kind === "file") {
+          files.push(dt.items[i].getAsFile());
+        }
+      }
+    } else { // In case of old implementation
+      for (let i=0; i < dt.files.length; i++) {
+        console.log("... file[" + i + "].name = " + dt.files[i].name);
+      }
+    }
+
+    console.log(this.props);
+    this.props.onReceiveImages(files);
+  }
+
+  dragend_handler = (event) => {
+    console.log('DRAGEND EVENT');
+    // Remove all of the drag data
+    var dt = event.dataTransfer;
+    if (dt.items) {
+      // Use DataTransferItemList interface to remove the drag data
+      for (var i = 0; i < dt.items.length; i++) {
+        dt.items.remove(i);
+      }
+    } else {
+      // Use DataTransfer interface to remove the drag data
+      event.dataTransfer.clearData();
+    }
   }
 
   render() {
@@ -57,9 +52,9 @@ class Stream extends Component {
     return (
       <div
         id='stream'
-        onDrop={event => drop_handler(this.props, event)}
-        onDragOver={event => dragover_handler(this.props, event)}
-        onDragEnd={event => dragend_handler(this.props, event)}
+        onDrop={this.drop_handler}
+        onDragOver={this.dragover_handler}
+        onDragEnd={this.dragend_handler}
       >
         IMAGE STREAM HERE
       </div>
