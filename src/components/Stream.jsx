d9db57f4a33ec70c16b0135b4029adba7d521ef6
Michael Sodaro <xtra.zero@gmail.com>
2017-08-11 16:27:18 +0900

    Handle all stages of drag event

--- src/components/Stream.jsx
+++ src/components/Stream.jsx
@@ -6,15 +6,49 @@ const dragover_handler = (event) => {
   console.log('DRAGOVER EVENT', event);
 }
 
-const receivedImage = (event) => {
+const drop_handler = (event) => {
   event.preventDefault();
   console.log('RECEIVED IMAGE EVENT', event);
+
+  var dt = event.dataTransfer;
+  if (dt.items) {
+    for (var i=0; i < dt.items.length; i++) {
+      if (dt.items[i].kind === "file") {
+        var f = dt.items[i].getAsFile();
+        console.log("... file[" + i + "].name = " + f.name);
+      }
+    }
+  } else {
+    for (var j=0; j < dt.files.length; j++) {
+      console.log("... file[" + i + "].name = " + dt.files[j].name);
+    }
+  }
+}
+
+const dragend_handler = (event) => {
+  console.log('DRAGEND EVENT');
+  // Remove all of the drag data
+  var dt = event.dataTransfer;
+  if (dt.items) {
+    // Use DataTransferItemList interface to remove the drag data
+    for (var i = 0; i < dt.items.length; i++) {
+      dt.items.remove(i);
+    }
+  } else {
+    // Use DataTransfer interface to remove the drag data
+    event.dataTransfer.clearData();
+  }
 }
 
 class Stream extends Component {
   render() {
     return (
-      <div id='stream' onDrop={receivedImage} onDragOver={dragover_handler}>
+      <div
+        id='stream'
+        onDrop={drop_handler}
+        onDragOver={dragover_handler}
+        onDragEnd={dragend_handler}
+      >
         IMAGE STREAM HERE
       </div>
     );
@@ -23,7 +57,7 @@ class Stream extends Component {
 
 Stream.propTypes = {
   images: PropTypes.array.isRequired,
-  //receivedImage: PropTypes.func.isRequired
+  //receivedImages: PropTypes.func.isRequired
 }
 
 export default Stream;
