630f5d8eebda9fa389a1cdb499df427a832e3557
Michael Sodaro <xtra.zero@gmail.com>
2017-08-11 12:35:33 +0900

    onDragOver event fires

--- src/components/Stream.jsx
+++ src/components/Stream.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const dragover_handler = (event) => {
+  console.log('DRAGOVER EVENT', event);
+}
+
+const receivedImage = (event) => {
+  console.log('RECEIVED IMAGE EVENT', event);
+}
+
 class Stream extends Component {
   render() {
     return (
-      <div id='stream'>
+      <div id='stream' onDrop={receivedImage} onDragOver={dragover_handler}>
         IMAGE STREAM HERE
       </div>
     );
@@ -12,7 +20,8 @@ class Stream extends Component {
 }
 
 Stream.propTypes = {
-  images: PropTypes.array.isRequired
+  images: PropTypes.array.isRequired,
+  //receivedImage: PropTypes.func.isRequired
 }
 
 export default Stream;
