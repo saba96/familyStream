1670ee9b12b8b2fc487394eea28c6ab4b0f16ba0
Michael Sodaro <xtra.zero@gmail.com>
2017-08-12 15:02:23 +0900

    POST to redis working, GET from redis working

--- server/routes/images.js
+++ server/routes/images.js
@@ -1,13 +1,26 @@
+var redis = require('redis');
+var client = redis.createClient();
 var express = require('express');
 var router = express.Router();
 
+const THIRTY_SECONDS_IN_MS = 30000;
+
 /* GET images listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  client.zrangebyscore('images', Date.now(), Date.now() + THIRTY_SECONDS_IN_MS, (err, images) => {
+    if(err)
+      console.log(err);
+    else {
+      console.log(images)
+      res.send(images);
+    }
+  });
 });
 
 /* POST images */
 router.post('/', function(req, res, next) {
+  console.log(req.body.image);
+  client.zadd('images', Date.now() + THIRTY_SECONDS_IN_MS, req.body.image);
   res.send('POST acknowledged.');
 });
 
