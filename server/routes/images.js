04a0f3376596617c5144329c9f71d4e16bae161f
Michael Sodaro <xtra.zero@gmail.com>
2017-08-12 10:57:24 +0900

    Rename users endpoint to images, allow POST

--- server/routes/images.js
+++ server/routes/images.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET images listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+/* POST images */
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  res.send('post recognized');
 });
 
 module.exports = router;
